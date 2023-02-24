9a7b30429791fe85aeb8713eeb6d44e9759e1488
machao07 <machao_07@163.com>
2023-02-24 10:11:56 +0800

    添加axios&配置代理

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'https://www.baidu.com',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '')
+            }
+        }
+    }
 })
