67f0079ab22365ddcaa5bb07eca1b7b871f4e3ab
machao07 <machao_07@163.com>
2023-02-27 16:15:31 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { memo, Suspense } from 'react'
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import routes from "./router";
-import { Divider, Loading } from 'react-vant';
+import { Loading } from 'react-vant';
 import './App.less'
 
 export default memo(() => (
